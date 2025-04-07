628d0547ccc6bb64297e0cfd4fd30470462e9d66
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-04-07 17:02:17 -0300

    Quarto Commit

--- inverterpalavras/src/main/java/inverterpalavras/Main.java
+++ inverterpalavras/src/main/java/inverterpalavras/Main.java
@@ -3,6 +3,12 @@ import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
         String texto = JOptionPane.showInputDialog("Digite uma palavra/texto para inverter: ");
+
+        String otxet = inverterTexto(texto);
+
+        JOptionPane.showMessageDialog(null, otxet, "Palavra invertida", JOptionPane.PLAIN_MESSAGE);
+
+
     }
     public static String inverterTexto(String texto)
     {
