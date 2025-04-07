f3f0111791a1f4c42779c64eae5bfd0b5d3e93c2
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-04-07 16:59:54 -0300

    Terceiro Commit

--- inverterpalavras/src/main/java/inverterpalavras/Main.java
+++ inverterpalavras/src/main/java/inverterpalavras/Main.java
@@ -4,5 +4,14 @@ public class Main {
     public static void main(String[] args) {
         String texto = JOptionPane.showInputDialog("Digite uma palavra/texto para inverter: ");
     }
-
+    public static String inverterTexto(String texto)
+    {
+        int tamanho = texto.length() - 1;
+        String palavraInvertida = "";
+        for (int i = tamanho; i >= 0; i--)
+        {
+            palavraInvertida += texto.charAt(i);
+        }
+        return palavraInvertida;
+    }
 }
